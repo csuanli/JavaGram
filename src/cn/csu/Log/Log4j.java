dc6c4c84d6f453290b655ada59f083e3f6af7857
csuanli <csuanli@163.com>
2017-06-27 11:30:33 +0800

    reason:add log4j test

--- src/cn/csu/Log/Log4j.java
+++ src/cn/csu/Log/Log4j.java
@@ -1,9 +1,16 @@
 package cn.csu.Log;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 public class Log4j {
 
 	public static void main(String[] args) {
-
+	    PropertyConfigurator.configure("C:/Users/Administrator/git/JavaGram/src/log4j.properties");
+	    Logger logger = Logger.getLogger(Log4j.class);
+	    logger.debug("why 1");
+	    logger.error("why 2");
+	    logger.info("why 3");
 	}
 
 }
